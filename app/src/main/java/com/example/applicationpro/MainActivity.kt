a9f4c8638e13b6fe8f1c973455d16f172836b5bb
TinhCaiWinDao <84382773+TinhCaiWinDao@users.noreply.github.com>
2023-03-29 17:09:43 +0700

    LoginActivity

--- app/src/main/java/com/example/applicationpro/MainActivity.kt
+++ app/src/main/java/com/example/applicationpro/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.applicationpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.applicationpro.databinding.ActivityMainBinding
+import com.example.applicationpro.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        this.binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
