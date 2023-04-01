cfe5a31fb2d225908e52605e5729edfd45521ab5
TinhCaiWinDao <84382773+TinhCaiWinDao@users.noreply.github.com>
2023-04-01 23:30:43 +0700

    Thiết kế giao diện :)))))

--- app/src/main/java/com/example/applicationpro/MainActivity.kt
+++ app/src/main/java/com/example/applicationpro/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.btnUser.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
